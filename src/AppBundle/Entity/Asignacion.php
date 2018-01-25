50c40fa0ba26bea1a1a5664c2a3e72cf89dd3c07
ema7nic <ema7nic@gmail.com>
2018-01-24 21:22:49 -0300

    Creo entidades
    creo entidades asignación y asignacion global

---
+++ src/AppBundle/Entity/Asignacion.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Asignacion
+ *
+ * @ORM\Table(name="asignacion")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AsignacionRepository")
+ */
+class Asignacion
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="monto", type="integer")
+     */
+    private $monto;
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_asignacion",type="datetime")
+     */
+    private $fechaAsignacion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="string", length=255)
+     */
+    private $descripcion;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set monto
+     *
+     * @param integer $monto
+     *
+     * @return Asignacion
+     */
+    public function setMonto($monto)
+    {
+        $this->monto = $monto;
+
+        return $this;
+    }
+
+    /**
+     * Get monto
+     *
+     * @return int
+     */
+    public function getMonto()
+    {
+        return $this->monto;
+    }
+
+    /**
+     * Set fechaAsignacion
+     *
+     * @param string $fechaAsignacion
+     *
+     * @return Asignacion
+     */
+    public function setFechaAsignacion($fechaAsignacion)
+    {
+        $this->fechaAsignacion = $fechaAsignacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaAsignacion
+     *
+     * @return string
+     */
+    public function getFechaAsignacion()
+    {
+        return $this->fechaAsignacion;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     *
+     * @return Asignacion
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+}
+
---
+++ src/AppBundle/Entity/AsignacionGlobal.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AsignacionGlobal
+ *
+ * @ORM\Table(name="asignacion_global")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AsignacionGlobalRepository")
+ */
+class AsignacionGlobal
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="monto", type="integer")
+     */
+    private $monto;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_asignacion", type="datetime")
+     */
+    private $fechaAsignacion;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="saldo", type="integer")
+     */
+    private $saldo;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     *
+     * @return AsignacionGlobal
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set monto
+     *
+     * @param integer $monto
+     *
+     * @return AsignacionGlobal
+     */
+    public function setMonto($monto)
+    {
+        $this->monto = $monto;
+
+        return $this;
+    }
+
+    /**
+     * Get monto
+     *
+     * @return int
+     */
+    public function getMonto()
+    {
+        return $this->monto;
+    }
+
+    /**
+     * Set fechaAsignacion
+     *
+     * @param \DateTime $fechaAsignacion
+     *
+     * @return AsignacionGlobal
+     */
+    public function setFechaAsignacion($fechaAsignacion)
+    {
+        $this->fechaAsignacion = $fechaAsignacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaAsignacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaAsignacion()
+    {
+        return $this->fechaAsignacion;
+    }
+
+    /**
+     * Set saldo
+     *
+     * @param integer $saldo
+     *
+     * @return AsignacionGlobal
+     */
+    public function setSaldo($saldo)
+    {
+        $this->saldo = $saldo;
+
+        return $this;
+    }
+
+    /**
+     * Get saldo
+     *
+     * @return int
+     */
+    public function getSaldo()
+    {
+        return $this->saldo;
+    }
+}
+
