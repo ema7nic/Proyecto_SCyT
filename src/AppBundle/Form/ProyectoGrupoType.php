736cd8fc3b9324b965fa90806a2f3a26528318cd
ema7nic <ema7nic@gmail.com>
2018-03-23 21:34:03 -0300

    actulizo proyecto type
    agrego el campo usaurio que mapea con la entidad usuario

--- src/AppBundle/Form/ProyectoGrupoType.php
+++ src/AppBundle/Form/ProyectoGrupoType.php
@@ -5,26 +5,34 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class ProyectoGrupoType extends AbstractType
-{
+class ProyectoGrupoType extends AbstractType {
+
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-        ->add('codigo')
-        ->add('codigoUtn')
-        ->add('nombre')
-        ->add('fechaInicio')
-        ->add('fechaFin')
-        ->add('usuario');
-    }/**
+                ->add('codigo')
+                ->add('codigoUtn')
+                ->add('nombre')
+                ->add('fechaInicio')
+                ->add('fechaFin')
+                ->add('nombre')
+                ->add('saldo');
+
+        $builder->add('usuario', EntityType::class, array(
+            'class' => 'AppBundle:Usuario',
+            'choice_label' => 'nombre',
+        ));
+    }
+
+/**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\ProyectoGrupo'
         ));
@@ -33,10 +41,8 @@ class ProyectoGrupoType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'appbundle_proyectogrupo';
     }
-
 
 }
